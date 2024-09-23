ee8e889c06b2c72c17e949ccad5d3fcb0e2a0712
Draunzler <54343288+draunzler@users.noreply.github.com>
2024-09-24 02:32:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,4 +69,5 @@ def index():
                            city_date=city_date)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8000)
